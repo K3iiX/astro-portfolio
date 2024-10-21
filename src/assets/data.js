8f30378a94f3e950dacf2b72e97f3ee055af4bff
K3iiX <karaya@marandino.dev>
2024-10-21 13:39:46 -0600

    initial commit and start navbar works

---
+++ src/assets/data.js
@@ -0,0 +1,17 @@
+export const socials = [
+    {
+        logo: 'bx bxl-youtube',
+    },
+    {
+        logo: 'bx bxl-linkedin-square',
+    },
+    {
+        logo: 'bx bxl-instagram',
+    },
+    {
+        logo: 'bx bxl-facebook-circle'
+    },
+    {
+        logo: 'bx bxl-github'
+    }
+]
