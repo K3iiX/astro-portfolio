827f83cc61088dd9769787dc969e01725b9c4958
K3iiX <karaya@marandino.dev>
2024-10-21 15:32:34 -0600

    feat: complete navbar component

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,12 @@
 export const socials = [
-    {
-        logo: 'bx bxl-youtube',
-    },
-    {
-        logo: 'bx bxl-linkedin-square',
-    },
-    {
-        logo: 'bx bxl-instagram',
-    },
-    {
-        logo: 'bx bxl-facebook-circle'
-    },
-    {
-        logo: 'bx bxl-github'
-    }
+    { logo: 'bx bxl-youtube'},
+    { logo: 'bx bxl-linkedin-square'},
+    { logo: 'bx bxl-instagram' },
+    { logo: 'bx bxl-facebook-circle' },
+    { logo: 'bx bxl-github'}
 ]
+
+export const info = [
+    { text:'About' },
+    { text:'Projects' }
+];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				xs: '280px',
+				xxs: '400px',
+			}
+		},
 	},
 	plugins: [],
 }
