adb82ee54f898103ad19ff4743ee0b33fb972b6f
K3iiX <karaya@marandino.dev>
2024-10-22 11:03:58 -0600

    feat: technologies component with slider and modal animations

--- src/assets/data.js
+++ src/assets/data.js
@@ -10,3 +10,62 @@ export const info = [
     { text:'About' },
     { text:'Projects' }
 ];
+
+export const technologies = [
+    {
+        text: 'TypeScript',
+        logo: 'bx bxl-typescript',
+    },
+    {
+        text: 'JavaScript',
+        logo: 'bx bxl-javascript',
+    },
+    {
+        text: 'React',
+        logo: 'bx bxl-react',
+    },
+    {
+        text: 'PostgreSQL',
+        logo: 'bx bxl-postgresql',
+    },
+    {
+        text: 'NodeJS',
+        logo: 'bx bxl-nodejs',
+    },
+    {
+        text: 'MongoDB',
+        logo: 'bx bxl-mongodb',
+    },
+    {
+        text: 'TailwindCSS',
+        logo: 'bx bxl-tailwind-css',
+    },
+    {
+        text: 'Bootstrap',
+        logo: 'bx bxl-bootstrap',
+    },
+    {
+        text: 'Git',
+        logo: 'bx bxl-git',
+    },
+    {
+        text: 'Github',
+        logo: 'bx bxl-github',
+    },
+    {
+        text: 'Terminal',
+        logo: 'bx bxs-terminal',
+    },
+    {
+        text: 'HTML5',
+        logo: 'bx bxl-html5',
+    },
+    {
+        text: 'CSS3',
+        logo: 'bx bxl-css3',
+    },
+    {
+        text: 'SASS',
+        logo: 'bx bxl-sass',
+    },
+];
